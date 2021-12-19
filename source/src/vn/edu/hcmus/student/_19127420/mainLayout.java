bbb21b757fd4a87a0756b98f764dacb63f75b5fb
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-12-19 09:37:31 +0700

    add main layout

---
+++ source/src/vn/edu/hcmus/student/_19127420/mainLayout.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmus.student._19127420;
+
+import javax.swing.*;
+
+/**
+ * vn.edu.hcmus.student._19127420
+ * Created by HuynhBaHuy
+ * Date 12/19/2021 - 1:18 AM
+ * Description: ...
+ */
+public class mainLayout {
+    private JPanel mainLayout;
+}
