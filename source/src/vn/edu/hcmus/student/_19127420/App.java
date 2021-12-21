9f44b3e5df9c6a592f11b996573586291e08e2cb
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2021-12-21 21:08:54 +0700

    [Draft] random feature

--- source/src/vn/edu/hcmus/student/_19127420/App.java
+++ source/src/vn/edu/hcmus/student/_19127420/App.java
@@ -1,6 +1,5 @@
 package vn.edu.hcmus.student._19127420;
 
-import vn.edu.hcmus.student._19127420.layout.MainFrame;
 import vn.edu.hcmus.student._19127420.layout.layout;
 
 public class App {
--- source/src/vn/edu/hcmus/student/_19127420/layout/layout.java
+++ source/src/vn/edu/hcmus/student/_19127420/layout/layout.java
@@ -6,15 +6,15 @@ package vn.edu.hcmus.student._19127420.layout;/*..
  */
 
 import vn.edu.hcmus.student._19127420.data.dictionary;
+import vn.edu.hcmus.student._19127420.data.slangWord;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class layout extends JFrame {
-    JPanel menuPanel;
-    JPanel headerPanel;
-
-
+public class layout extends JFrame  implements ActionListener {
+    JPanel bodyPanel;
     JButton searchButton;
     JButton settingButton;
     JButton quizzButton;
@@ -22,21 +22,81 @@ public class layout extends JFrame {
     JButton backButton;
     dictionary data;
 
-    class menuPanel extends JPanel{
+    class randomPanel extends JPanel implements ActionListener {
+        JPanel headerPanel;
+        JButton randBtn;
+        JPanel bodyPanel;
+        JLabel slangLabel;
+        JList<String> meanings;
+        slangWord randSw;
+        public randomPanel()
+        {
+            setLayout(new BorderLayout());
+
+            // header panel
+            headerPanel = new JPanel(new FlowLayout());
+            randBtn = new JButton("Random");
+            headerPanel.add(randBtn);
+            headerPanel.add(backButton);
+
+            // add action listener for button
+            randBtn.addActionListener(this);
+
+
+            // prepare data
+            slangWord randSw = data.randomSlang();
+            slangLabel = new JLabel(randSw.getSlang(),SwingConstants.CENTER);
+            meanings = new JList<String>();
+            meanings.setListData(randSw.getMeaning());
+            meanings.setPreferredSize(new Dimension(10,20));
+
+            //body panel
+            bodyPanel = new JPanel(new BorderLayout());
+            bodyPanel.add(slangLabel,BorderLayout.PAGE_START);
+            bodyPanel.add(meanings,BorderLayout.CENTER);
+
+
+
+            add(headerPanel,BorderLayout.PAGE_START);
+            add(bodyPanel,BorderLayout.CENTER);
+        }
+        private void loadBodyPanel() {
+            slangLabel.setText(randSw.getSlang());
+            meanings.setListData(randSw.getMeaning());
+
+
+        }
+        public void clear(){
+            slangLabel.setText("");
+            meanings.removeAll();
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //clear old data
+            clear();
+            // prepare data
+            randSw = data.randomSlang();
+            // load data
+            loadBodyPanel();
+        }
+    }
+
+    class menuPanel extends JPanel {
         JLabel menuLabel;
         JPanel bodyPanel;
         public menuPanel(){
             setLayout(new BorderLayout());
-            menuLabel = new JLabel("Menu");
-            bodyPanel = new JPanel(new GridBagLayout());
-
-            GridBagConstraints constraints = new GridBagConstraints();
-            
-
-
+            menuLabel = new JLabel("Menu",SwingConstants.CENTER);
+            menuLabel.setAlignmentX(Label.CENTER);
+            bodyPanel = new JPanel(new GridLayout(4,1,5,10));
+            bodyPanel.add(randomButton);
+            bodyPanel.add(searchButton);
+            bodyPanel.add(settingButton);
+            bodyPanel.add(quizzButton);
             add(menuLabel, BorderLayout.PAGE_START);
             add(bodyPanel, BorderLayout.CENTER);
         }
+
     }
 
     public layout(){
@@ -46,27 +106,63 @@ public class layout extends JFrame {
         //initialize the component
         initializeComponent();
 
+        // body panel
+        bodyPanel = new JPanel(new CardLayout());
 
+        // initizliza the menu pane
+        JPanel menuPanel = new menuPanel();
+        JPanel randomPanel = new randomPanel();
 
+        bodyPanel.add(menuPanel,"menu");
+        bodyPanel.add(randomPanel,"random");
         // show GUI
-        createAndShowGUI();
+        createAndShowGUI(bodyPanel);
     }
 
     private void initializeComponent(){
+        // init
         searchButton = new JButton("Search");
         settingButton = new JButton("Settings");
         quizzButton = new JButton("Quizz");
         randomButton = new JButton("Random");
         backButton = new JButton("Back");
-    }
 
-    private void createAndShowGUI(){
+        // action lister
+        randomButton.addActionListener(this);
+        randomButton.setActionCommand("random_menu_btn");
+        backButton.addActionListener(this);
+        backButton.setActionCommand("back");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String str = e.getActionCommand();
+        CardLayout cl = (CardLayout)(bodyPanel.getLayout());
+        if(str == "random_menu_btn"){
+            cl.show(bodyPanel,"random");
+        }
+        else if(str == "search_menu_btn"){
+
+        }
+        else if(str == "setting_menu_btn"){
+
+        }
+        else if(str == "quizz_menu_btn"){
+
+        }
+        else if(str == "back")
+        {
+            cl.previous(bodyPanel);
+        }
+    }
+    private void createAndShowGUI(Container container){
         JFrame.setDefaultLookAndFeelDecorated(true);
 
         setTitle("Slang Dictionary");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(menuPanel);
+        setContentPane(container);
         pack();
+        setSize(new Dimension(500,300));
+        setLocation(new Point(500,150));
         setVisible(true);
     }
 }
